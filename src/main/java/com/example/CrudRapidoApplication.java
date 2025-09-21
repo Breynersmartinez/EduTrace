4629abae196122bea1a76a287e96593775d99a22
Breiner Saul Martinez Muñoz <153401516+Breynersmartinez@users.noreply.github.com>
2025-09-21 17:19:41 -0500

    Update CrudRapidoApplication.java

--- src/main/java/com/example/CrudRapidoApplication.java
+++ src/main/java/com/example/CrudRapidoApplication.java
@@ -8,7 +8,7 @@ public class CrudRapidoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudRapidoApplication.class, args);
-		
+		System.out.println(" This API developed in Spring Boot allows the management of students, projects, teams, and tasks. Its purpose is to provide traceability of teamwork, ensuring that each member has evidence of their progress and responsibilities.");
 	}
 
 }
